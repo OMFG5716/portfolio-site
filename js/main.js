35a74f0b093fb6f8989e1feef0883f593088fd2a
Jivanysh Sohoni <jivanyesh@gmail.com>
2015-10-29 14:57:20 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+// Pace.on('done',function(){
+
+// 	$('header, .grid-item').addClass('animated fadeInUp');
+// });
+
+
+// Pace.on('done', function(){
+// });
+
+
+$('.view-work').magnificPopup({
+	type: "image",
+	image:{
+		verticalFit: false
+	}
+})
+
+new WOW({
+	mobile: false
+}).init();
+
